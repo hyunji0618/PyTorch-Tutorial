0893fc323637037859e92d45b0fd326b129c8df6
hyunjikim <hyunjik000618@gmail.com>
2023-04-15 21:37:24 +0900

    autograd revision

--- autograd.py
+++ autograd.py
@@ -15,3 +15,29 @@ print(z)
 # calculating gradient of z with respect to x
 z.backward() # dz/dx
 print(x.grad) # tensor([3.7406, 3.6692, 4.1282])
+
+# disable pytorch from using the gradient function
+x.requires_grad_(False)
+y = x.detach()
+with torch.no_grad():
+    y = x * 2
+    
+# Training Example
+weights = torch.ones(4, requires_grad=True)
+
+for epoch in range(3):
+    model_output = (weights * 3).sum()
+    model_output.backward()
+    print(weights.grad)
+    weights.grad.zero_()
+    
+# tensor([3., 3., 3., 3.])
+# tensor([3., 3., 3., 3.])
+# tensor([3., 3., 3., 3.]) 
+
+# Optimization
+weights = torch.ones(4, requires_grad=True)
+
+optimizer = torch.optim.SGD(weights, lr = 0.01)
+optimizer.step()
+optimizer.zero_grad()
