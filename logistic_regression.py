43e591dec03266af85a2dc6fb7ee67d805f6168b
hyunjikim <hyunjik000618@gmail.com>
2023-04-21 16:25:17 +0900

    logistic regression 02

--- logistic_regression.py
+++ logistic_regression.py
@@ -21,36 +21,22 @@ x, y = bc.data, bc.target
 
 n_samples, n_features = x.shape
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
 
 # 1) Design model
-input_size = n_features
-output_size = 1
-model = nn.Linear(input_size, output_size)
+
 
 # 2) Construct loss and optimizer
-learning_rate = 0.01
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
 
 # 3) Training loop
-num_epochs = 100
-for epoch in range(num_epochs):
+
     # forward pass and loss
-    y_predicted = model(X)
-    loss = criterion(y_predicted, Y)
+
     
     # backward pass
-    loss.backward()
     
     # update the progress
-    optimizer.step()
-    optimizer.zero_grad()
-    
-    if (epoch + 1) % 10 == 0:
-        print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
+
         
 # plot
-predicted = model(X).detach().numpy()
-plt.plot(x_numpy, y_numpy, 'ro')
-plt.plot(x_numpy, predicted, 'b')
-plt.show()
