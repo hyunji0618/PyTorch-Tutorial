cf1eba3e300b5cb66e71223bbc91754b61a97e05
hyunjikim <hyunjik000618@gmail.com>
2023-04-24 22:28:30 +0900

    .

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,7 +11,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from sklearn import datasets # generate regression dataset
+from sklearn import datasets 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
